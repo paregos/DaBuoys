cb17ff2d0269459892eb5bb3e7282a859afda287
Event <event@mds.co.nz>
2017-01-21 11:16:55 +1300

    implement uv for dynamic mesh

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        WaveMesh.CreateWave(5, 10, 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/WaveController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour {
+
+    public Vector3[] newVertices;
+    public Vector2[] newUV;
+    public int[] newTriangles;
+    void Start()
+    {
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        mesh.vertices = newVertices;
+        mesh.uv = newUV;
+        mesh.triangles = newTriangles;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        Vector3[] normals = mesh.normals;
+        int i = 0;
+        while (i < vertices.Length)
+        {
+            vertices[i] += normals[i] * Mathf.Sin(Time.time);
+            i++;
+        }
+        mesh.vertices = vertices;
+    }
+}
