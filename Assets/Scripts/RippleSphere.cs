55702b650a80bd271ece846fee69e66bb50b21c2
Event <event@mds.co.nz>
2017-01-21 19:13:41 +1300

    Adds reflection prototype

---
+++ Assets/Scripts/RippleSphere.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RippleSphere : MonoBehaviour {
+
+    private SphereCollider collider;
+    public float speed = 3f;
+	// Use this for initialization
+	void Start () {
+        collider = GetComponent<SphereCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        collider.radius += speed * Time.deltaTime;
+	}
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("ENTERED");
+        if (other.tag == "Rock")
+        {
+            Destroy(other.gameObject);
+        }
+        
+    }
+
+}
