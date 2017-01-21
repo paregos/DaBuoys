2082eb583d2a968f535d0d354cd0d0c530e78eb5
Event <event@mds.co.nz>
2017-01-22 00:35:56 +1300

    Implements Poseidon Player and attack beam

---
+++ Assets/Scripts/PoseidonController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoseidonController : MonoBehaviour {
+    public Vector3 spawnPosition = new Vector3(0, 10, 0);
+    public float dropDistanceBelowPoseidon = 3f;
+
+    // Select which prefab should be dropped from ingame GUI
+    public Transform prefab;
+
+	// Use this for initialization
+	void Start () {
+        transform.position = spawnPosition;
+    }
+
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    public float chargeTime = 5.0f;
+    private float chargeCount = 0f;
+    private bool charged = false;
+    void Update()
+    {
+        chargeCount += Time.deltaTime;
+        CharacterController controller = GetComponent<CharacterController>();
+ 
+        moveDirection = Quaternion.Euler(0, 45, 0) * new Vector3(Input.GetAxis("P2-H"), 0, Input.GetAxis("P2-V"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+        controller.Move(moveDirection * Time.deltaTime);
+
+        chargeShot(chargeCount);
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Hit");
+            if (charged)
+            {
+                drop(prefab);
+                chargeCount = 0f;
+                charged = false;
+            }
+        }   
+    }
+
+    private void drop(Transform prefabToDrop)
+    {
+        Instantiate(prefabToDrop, transform.position + Vector3.down * dropDistanceBelowPoseidon, Quaternion.identity);
+    }
+
+    private float startAngle = 179f;
+    private float endAngle = 20f;
+
+    /**
+        Called everyframe to focus the shot beam.
+    */
+    private void chargeShot(float chargeCount)
+    {
+        float clamped = Mathf.Clamp01(chargeCount / chargeTime);
+        float spotLightAngle = Mathf.Lerp(startAngle, endAngle, clamped);
+        Light spotLight = GetComponentInChildren<Light>();
+        spotLight.spotAngle = spotLightAngle;
+        if (clamped == 1 && !charged)
+        {
+            shotCharged();
+        }
+    }
+    /**
+        Called to initiate the shot charged animation.
+    */
+    private void shotCharged()
+    {
+        charged = true;
+        StartCoroutine(flash());
+    }
+
+    private float offLightRange = 10f;
+    private float onLightRange = 50f;
+    IEnumerator flash()
+    {
+        Light spotLight = GetComponentInChildren<Light>();
+        float initialRange = spotLight.range;
+        spotLight.range = 10f;
+        yield return new WaitForSeconds(0.1f);
+        spotLight.range = 50f;
+        yield return new WaitForSeconds(0.1f);
+        spotLight.range = 10f;
+        yield return new WaitForSeconds(0.1f);
+        spotLight.range = 50f;
+        yield return new WaitForSeconds(0.1f);
+        spotLight.range = 10f;
+        yield return new WaitForSeconds(0.1f);
+        spotLight.range = 50f;
+        yield return new WaitForSeconds(0.1f);
+        spotLight.range = initialRange;
+    }
+}
--- Assets/Scripts/RockGenerator.cs
+++ Assets/Scripts/RockGenerator.cs
@@ -6,7 +6,7 @@ public class RockGenerator : MonoBehaviour {
 
 	public GameObject rock;
 	public GameObject target;
-	public float y;
+	public float dropHeight;
 
 	Ray ray;
 	RaycastHit hit;
@@ -16,21 +16,36 @@ public class RockGenerator : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-		obj = Instantiate (rock, new Vector3 (hit.point.x, hit.point.y + y, hit.point.z), Quaternion.identity) as GameObject;
+		obj = Instantiate (rock, new Vector3 (hit.point.x, hit.point.y + dropHeight, hit.point.z), Quaternion.identity) as GameObject;
 		targ = Instantiate (target, new Vector3 (hit.point.x, hit.point.y, hit.point.z), Quaternion.identity) as GameObject;
 	}
-	
+
+    public Plane plan;
 	// Update is called once per frame
 	void Update () {
-		ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+        /*ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 		if (Physics.Raycast (ray, out hit)) {
 			if (Input.GetMouseButtonUp(0)) {
-				GameObject targ = Instantiate (rock, new Vector3 (hit.point.x, hit.point.y + y, hit.point.z), Quaternion.identity) as GameObject;
+				GameObject targ = Instantiate (rock, new Vector3 (hit.point.x, hit.point.y + dropHeight, hit.point.z), Quaternion.identity) as GameObject;
 			}
-		}
-		Vector3 temp = Input.mousePosition;
+		}*/
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        int layer_mask = LayerMask.GetMask("NoObjectCollision");
+        if (Physics.Raycast(ray, out hit, 100.0f, layer_mask))
+        {
+            Debug.Log(hit.point);
+        }
+
+        /*
+        Vector3 temp = Input.mousePosition;
 		temp.z = 12f;
 		targ.transform.position = Camera.main.ScreenToWorldPoint (temp);
-		//targ.transform.position = Camera.main.ScreenToWorldPoint (new Vector3 (hit.point.x, 0f, hit.point.z));
-	}
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 dropPosition = targ.transform.position + Vector3.up * dropHeight;
+            Instantiate(rock, dropPosition, Quaternion.identity);
+        }
+        */
+        //targ.transform.position = Camera.main.ScreenToWorldPoint (new Vector3 (hit.point.x, 0f, hit.point.z));
+    }
 }
