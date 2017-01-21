7192a8a18d52c8f8be7a9bce4b0d5c299551c1db
Event <event@mds.co.nz>
2017-01-22 04:27:05 +1300

    Adds dissipate option to PlopEffect and adds plops to bass

--- Assets/Scripts/Bass.cs
+++ Assets/Scripts/Bass.cs
@@ -3,9 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bass : MonoBehaviour {
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public Transform PlopEffect;
+    public float timeToPlop = 3f;
+    public float killTime = 9f;
+    public float plopTime = 0.8f;
+    public float plopForce = 3f;
+    private float plopCount = 0f;
+    private float timeSteps = 0.1f;
+    private Vector3 startPos;
+
+    void Start()
+    {
+        StartCoroutine(startPlop());
+        StartCoroutine(kill());
+        startPos = transform.position;
+    }
+
+    IEnumerator kill()
+    {
+        yield return new WaitForSeconds(killTime);
+        Debug.Log("Bass Ded");
+        Destroy(gameObject);
+    }
+
+    IEnumerator startPlop()
+    {
+        Debug.Log("Bass plop");
+        yield return new WaitForSeconds(timeToPlop);
+        Transform trans = Instantiate(PlopEffect, startPos, Quaternion.identity) as Transform;
+        Plop plop = trans.gameObject.GetComponent<Plop>();
+        plop.startDissipate(timeToPlop);
+    }
 }
