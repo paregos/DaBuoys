fe7e630e377bc26e6e9939d136eda99418d04a8e
Jeff <mmu119@aucklanduni.ac.nz>
2017-01-21 15:48:29 +0900

    Implement droping object feature

---
+++ Assets/Scripts/RockGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockGenerator : MonoBehaviour {
+
+	public GameObject rock;
+	public GameObject target;
+	public float y;
+
+	Ray ray;
+	RaycastHit hit;
+	GameObject obj;
+	GameObject targ;
+
+	// Use this for initialization
+	void Start () {
+		ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		obj = Instantiate (rock, new Vector3 (hit.point.x, hit.point.y + y, hit.point.z), Quaternion.identity) as GameObject;
+		targ = Instantiate (target, new Vector3 (hit.point.x, hit.point.y, hit.point.z), Quaternion.identity) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		if (Physics.Raycast (ray, out hit)) {
+			if (Input.GetMouseButtonUp(0)) {
+				GameObject targ = Instantiate (rock, new Vector3 (hit.point.x, hit.point.y + y, hit.point.z), Quaternion.identity) as GameObject;
+			}
+		}
+		Vector3 temp = Input.mousePosition;
+		temp.z = 12f;
+		targ.transform.position = Camera.main.ScreenToWorldPoint (temp);
+		//targ.transform.position = Camera.main.ScreenToWorldPoint (new Vector3 (hit.point.x, 0f, hit.point.z));
+	}
+}
