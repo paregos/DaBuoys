52d63c15be72f7e56fbc476e817c105959e0ad21
Event <event@mds.co.nz>
2017-01-21 18:39:01 +1300

    Adds our good friend BoatyMcBoatface

---
+++ Assets/Scripts/Boaty.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boaty : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+        if ( Mathf.Abs(transform.rotation.x) > 90f || Mathf.Abs(transform.rotation.y) > 90f || Mathf.Abs(transform.rotation.z) > 90f)
+        {
+
+        }
+	}
+
+    private void kill()
+    {
+        Debug.Log("RIP McBoaty :(");
+        Destroy(gameObject);
+    }
+}
