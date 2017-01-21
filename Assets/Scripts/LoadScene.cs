5f2fa86cafc5420b92aff0f65afe76ac1b17130d
Jeff <mmu119@aucklanduni.ac.nz>
2017-01-21 09:35:21 +0900

    Creat Main Menu

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+	public void Load(string name){
+		SceneManager.LoadScene (name);
+	}
+
+	public void Quit(){
+		Application.Quit();
+	}
+}
